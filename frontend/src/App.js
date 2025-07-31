ba6fb85ed5b473dc370f6763c2dec27aee8741f8
traqian <24074691g@connect.polyu.hk>
2025-08-01 00:42:00 +0800

    简化应用，移除复杂组件

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-// Components
-import CampusMap from './components/CampusMap';
-import BuildingInfo from './components/BuildingInfo';
-import Navigation from './components/Navigation';
-import Facilities from './components/Facilities';
-import RealTimeTracking from './components/RealTimeTracking';
-import UserInteraction from './components/UserInteraction';
-
 function App() {
   return (
     <Router>
       <div className="App">
-        <UserInteraction />
         <Routes>
-          <Route path="/" element={<CampusMap />} />
-          <Route path="/building/:id" element={<BuildingInfo />} />
-          <Route path="/navigation" element={<Navigation />} />
-          <Route path="/facilities" element={<Facilities />} />
-          <Route path="/tracking" element={<RealTimeTracking />} />
+          <Route path="/" element={<h1>校园地理信息系统</h1>} />
         </Routes>
       </div>
     </Router>
